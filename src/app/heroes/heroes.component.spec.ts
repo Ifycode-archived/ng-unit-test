c2e45c8bbf7442603b8e0484f909f1a30a31c9db
ifycode <marychord@gmail.com>
2020-12-21 21:55:08 -0800

    Testing components & Mocking to isolate code - Isolated testing

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,31 @@
+import { HeroesComponent } from "./heroes.component";
+import { of } from "rxjs";
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+  let mockHeroService;
+
+  beforeEach(() => {
+    HEROES = [
+      {id: 1, name: 'spiderDude', strength: 8},
+      {id: 2, name: 'WonderWoman', strength: 24},
+      {id: 3, name: 'superDude', strength: 55}
+    ]
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero']);
+    component = new HeroesComponent(mockHeroService);
+  });
+
+  describe('delete', () => {
+    it('should remove the indicated hero form the heroes list', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      //Act: delete the last item in the array
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2);
+    });
+  });
+});

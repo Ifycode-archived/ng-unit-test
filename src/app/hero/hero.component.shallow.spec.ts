cb058594671c609439850a20c37e4ea9106f7ec7
ifycode <marychord@gmail.com>
2021-01-29 19:30:06 -0800

    The TestBed

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,27 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { HeroComponent } from './hero.component';
+
+describe('HeroComponent (Shallow test)', () => {
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        HeroComponent
+      ]
+    });
+
+    fixture = TestBed.createComponent(HeroComponent);
+    //type fixture.componentInstance. to see the properties or methods on HeroComponent class
+  });
+
+  it('should have the correct hero', () => {
+    fixture.componentInstance.hero = {
+      id: 1,
+      name: 'SuperDude',
+      strength: 3
+    };
+
+    expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+  });
+});

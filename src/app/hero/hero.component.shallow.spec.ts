98934dff3e884f12f7ccdd705e3f995db0c8f60a
ifycode <marychord@gmail.com>
2021-01-29 19:35:52 -0800

    using NO_ERRORS_SCHEMA

--- src/app/hero/hero.component.shallow.spec.ts
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { HeroComponent } from './hero.component';
 
@@ -8,6 +9,9 @@ describe('HeroComponent (Shallow test)', () => {
     TestBed.configureTestingModule({
       declarations: [
         HeroComponent
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
       ]
     });
 

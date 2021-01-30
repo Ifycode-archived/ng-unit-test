b3793b99762d7488ca1b58fa45db10c59c16b742
ifycode <marychord@gmail.com>
2021-01-30 00:35:24 -0800

    Implementing a test with mocked http

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from "@angular/core/testing";
+import { TestBed, inject } from "@angular/core/testing";
 import { HeroService } from "./hero.service";
 import { MessageService } from "./message.service";
 import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
@@ -6,6 +6,7 @@ import { HttpClientTestingModule, HttpTestingController } from "@angular/common/
 describe('HeroService', () => {
   let mockMessageService;
   let httpTestingController: HttpTestingController;
+  let service: HeroService;
 
   beforeEach(() => {
     mockMessageService = jasmine.createSpyObj(['add']);
@@ -24,6 +25,31 @@ describe('HeroService', () => {
 
     httpTestingController = TestBed.get(HttpTestingController);
 
-    //Access service e.g. let msgSvc = TestBed.get(MessageService);
+    service = TestBed.get(HeroService);
+  });
+
+  describe('getHero', () => {
+    it('should call get with the correct URL', () => {
+
+      service.getHero(4).subscribe();
+
+      const req = httpTestingController.expectOne('api/heroes/4');
+      req.flush({
+        id: 4,
+        name: 'SuperDude',
+        strength: 100
+      });
+      httpTestingController.verify();
+    });
+
+    /*To use this method instead, remove everything that use TestBed.get above
+
+    it('should call get with the correct URL',
+      inject([HeroService, HttpTestingController],
+        (service: HeroService, controller: HttpTestingController) => {
+
+          service.getHero(4).subscribe();
+    }));
+    */
   });
 });

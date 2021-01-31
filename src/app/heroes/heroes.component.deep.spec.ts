fbdc35aaaf80b783b75c54f9e8b0581c199628da
ifycode <marychord@gmail.com>
2021-01-31 03:10:05 -0800

    Testing the routerlink - Note: () was omitted in onClick() earlier

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -8,7 +8,7 @@ import { By } from "@angular/platform-browser";
 
 @Directive({
   selector: '[routerLink]',
-  host: { '(click)': 'onClick' }
+  host: { '(click)': 'onClick()' }
 })
 export class RouterLinkDirectiveStub {
   @Input('routerLink') linkParams: any;
@@ -101,5 +101,21 @@ describe('HeroesComponent (deep test)', () => {
 
     const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
     expect(heroText).toContain(name);
+  });
+
+  it('should have the correct route for the first hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+    let routerLink = heroComponents[0].query(By.directive(RouterLinkDirectiveStub))
+      .injector.get(RouterLinkDirectiveStub);
+
+    heroComponents[0].query(By.css('a')).triggerEventHandler('click', null);
+
+    //console.log(routerLink);
+
+    expect(routerLink.navigatedTo).toBe('/detail/1');
   });
 });
